d1f8c1c804d22042bf2c1d8d1c0ae1f6b6cd5235
BeINone <sbeinone@gmail.com>
2018-10-13 20:43:35 +0900

    초기 환경 설정

---
+++ src/main/java/com/depromeet/clippingserver/ClippingServerApplication.java
@@ -0,0 +1,12 @@
+package com.depromeet.clippingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippingServerApplication.class, args);
+	}
+}
