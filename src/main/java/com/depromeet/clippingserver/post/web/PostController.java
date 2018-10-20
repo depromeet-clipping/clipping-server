b0127e12d79f3c45e994e805527e60045a43f7b8
BeINone <sbeinone@gmail.com>
2018-10-20 15:39:37 +0900

    [ADD] GetAllPostsResponse 클래스명 변경

--- src/main/java/com/depromeet/clippingserver/post/web/PostController.java
+++ src/main/java/com/depromeet/clippingserver/post/web/PostController.java
@@ -1,6 +1,6 @@
 package com.depromeet.clippingserver.post.web;
 
-import com.depromeet.clippingserver.post.domain.GetAllPostResponse;
+import com.depromeet.clippingserver.post.domain.GetAllPostsResponse;
 import com.depromeet.clippingserver.post.domain.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,7 @@ public class PostController {
     private PostRepository postRepository;
 
     @GetMapping
-    public GetAllPostResponse getAllPosts() {
-        return GetAllPostResponse.fromEntity(postRepository.findAll());
+    public GetAllPostsResponse getAllPosts() {
+        return GetAllPostsResponse.fromEntity(postRepository.findAll());
     }
 }
