0c506874643942785c611fb223fbedb6b228feed
Justin <anomie7777@gmail.com>
2018-10-21 15:02:35 +0900

    [data-h2.sql] write insert statment

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,1 +1,3 @@
-insert into post (post_id, category_id, comment, deleted, is_bookmark, source_of, title, url, user_id) values(null, null,'한국인',  FALSE, FALSE, 'naver', '한국인',  'hankook.com', null)
+insert into user(user_id, device_key) values(1, '#$9981')
+insert into post (post_id, category_id, comment, deleted, is_bookmark, source_of, title, url, user_id) values(null, null,'한국인1',  FALSE, FALSE, 'naver1', '한국인1',  'hankook.com', 1)
+insert into post (post_id, category_id, comment, deleted, is_bookmark, source_of, title, url, user_id) values(null, null,'한국인2',  FALSE, FALSE, 'naver2', '한국인2',  'hankook.com', 1)
